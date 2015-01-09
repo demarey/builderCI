2e92caae64e7e5d3f364568d592bc50bb04542c9
ephe-meral <j.appel.online@gmail.com>
2015-01-09 21:15:13 +0100

    changed url to point to sources directory, fixed 4.3 as well

--- build_client_image.sh
+++ build_client_image.sh
@@ -92,7 +92,7 @@ case "$ST" in
     wget -q http://ftp.squeak.org/4.3/Squeak4.3.zip
     unzip Squeak4.3.zip
     cd Squeak4.3
-    wget -q http://ftp.squeak.org/4.1/SqueakV41.sources.gz
+    wget -q http://ftp.squeak.org/sources_files/SqueakV41.sources.gz
     gunzip SqueakV41.sources.gz
     IMAGE_BASE_NAME=Squeak4.3
     ;;
@@ -129,7 +129,7 @@ case "$ST" in
     cd TrunkImage
     wget -q http://build.squeak.org/job/SqueakTrunk/lastSuccessfulBuild/artifact/target/TrunkImage.zip
     unzip TrunkImage.zip
-    wget -q http://ftp.squeak.org/4.5/SqueakV41.sources.gz
+    wget -q http://ftp.squeak.org/sources_files/SqueakV41.sources.gz
     gunzip SqueakV41.sources.gz
     IMAGE_BASE_NAME=TrunkImage
     ;;
